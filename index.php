237dd6a6c2b415292c4eb6fca4a491106f0768b5
Jack Slide <30323358+loisoj@users.noreply.github.com>
2018-07-14 18:52:54 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,22 +1,5 @@
 <?php
-
-// Kickstart the framework
 $black=require('system/lib/base.php');
-//$z=include('black.class.php');
-
-// CWAY BLACK
-// function magicway($cwayuri , $cwaylay){
-//
-//  $black->route('GET '.$cwayuri.'',
-// 	function($black) {
-//
-// 		echo View::instance()->render($cwaylay);
-// 	}
-//  );
-// }
-
-
-// Load configuration
 $black->config('black.cfg.ini');
 
 //Полный роут с функциями
